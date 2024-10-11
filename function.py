0465a13a606679c214c484bcec1b25d002114d6f
VTM-Planfarm <vinh@planfarm.com.au>
2024-10-11 09:31:10 +0800

    Fix the mispelling in naming

--- function.py
+++ function.py
@@ -4,7 +4,7 @@ import openpyxl
 import csv
 
 # Funtion to create a df from excel sheet
-def ToDataFrame(excel_wb):
+def ToDataFrame(excel_wb: str):
     # Read in the inventory sheet
     wb = openpyxl.load_workbook(excel_wb, 
                                 data_only=True)
@@ -32,14 +32,34 @@ def ToDataFrame(excel_wb):
         rows = globals()[f'row{i}']
         for f in range(len(rows)):
             ws.cell(i, f + 1).value = rows[f]
-            print(ws.cell(i, f + 1).value)
     # Written into a csv
-    with open('test.csv', 'w', newline="") as f:
+    with open('df.csv', 'w', newline="") as f:
         c = csv.writer(f)
         for r in ws.rows:
             c.writerow([cell.value for cell in r])        
     # convert into dataframe object  
-    df = pd.DataFrame(pd.read_csv('test.csv'))
+    df = pd.DataFrame(pd.read_csv('df.csv'))
     # Get rid of the extra row
     df = df.iloc[0:12]
     return df
+
+# Separate the big df into crop type
+def ByCropType(df: pd.DataFrame):
+    Crop = []
+    for i in range(len(df['Crop type'])):
+        Crop.append(df.iloc[i])
+    return Crop
+
+# Get the general information
+def GenInfo(excel_wb: str):
+    # Read in the excel wb
+    wb = openpyxl.load_workbook(excel_wb)
+    # Activate the sheet
+    ws = wb['General information']
+    # Get the state loc info (assuming the cell does not change)
+    loc = ws.cell(2, 2).value
+    # Rainfall > 600mm
+    rf_over = ws.cell(7, 2).value
+    # Production system
+    prod_sys = ws.cell(8, 2).value
+    return loc, rf_over, prod_sys
