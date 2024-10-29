f748a2a0f30bb5149037a69a9a2a57493980b96f
VTM-Planfarm <vinh@planfarm.com.au>
2024-10-29 10:35:26 +0800

    Function to extract follow up info

---
+++ From_q.py
@@ -0,0 +1,36 @@
+import csv
+import pandas as pd
+
+def follow_up(df: pd.DataFrame):
+    # Extract from csv
+    info = {}
+    info['List of on-farm machinery'] = df['If you have a list of all on-farm machinery and equipment, please upload it here. Alternatively, please email it to toby@terrawise.au'].iloc[0].split('\n')
+    info['Farm management software'] = df['Please list the applications you use below'].iloc[0].split('\n')
+    info['Variable rate'] = df['Do you utilise Variable Rate Technology (VRT) across your property? Or do you apply differing rates of fertiliser within paddock zones and/or crop types?'].iloc[0]
+    info['Record of variable rate'] = df['Are you happy to provide us with access to these applications, records and/or service providers to conduct your carbon account? If so, provide details via toby@terrawise.au or call 0488173271 for clarification'].iloc[0]
+    # Write out the follow up question
+    with open("follow_up.csv", 'w', newline='') as out:
+        csv_out = csv.DictWriter(out, info.keys())
+        csv_out.writeheader()
+        csv_out.writerow(info)
+
+def SpecCrop(df):
+    # Number of crop in the questionnaire
+    crops = df['What crops did you grow last year?'].iloc[0].split('\n')
+    # Loops for crop specific info
+    # based on the number of crops
+    for crop in crops:
+        crop_info = {}
+        for label, content in df.items():
+            if crop.lower() in label:
+                # Contracted services
+                if 'land management' in label:
+                    try:
+                        crop_info[f'Land management practices - {crop}'] = content.iloc[0].split('\n')
+                    except AttributeError:
+                        crop_info[f'{crop}'] = content.iloc[0]
+                try:
+                    out = pd.DataFrame(crop_info)
+                except ValueError:
+                    out = pd.DataFrame(crop_info, index=[0])
+                out.to_csv(f'{crop}_follow_up.csv')
