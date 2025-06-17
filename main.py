f8279f5465109d8c8872833ce738a1e274112ae1
VTM-Planfarm <vinh@planfarm.com.au>
2025-06-17 10:36:03 +0800

    Fix import statements by removing unnecessary lines and correcting module access

--- main.py
+++ main.py
@@ -11,14 +11,9 @@ import shutil, os, tempfile
 from datetime import datetime as dt
 import numpy as np
 import datetime as dt
-<<<<<<< HEAD
-from weather_stations import *
-import glob
-=======
 from functions.Extract_params import *
 from funcitons.From_q import *
 from functions.weather_stations import *
->>>>>>> 5816cc2 (Fix import statements for functions by correcting paths and ensuring proper module access)
 
 
 def remove_insert(list: list, index: int, value: str):
